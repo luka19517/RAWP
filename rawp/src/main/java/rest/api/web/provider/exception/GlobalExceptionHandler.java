1060d38caa367cf9c344677dc1c1648992a2fc27
luka19517 <luka.djorovic3010@gmail.com>
2024-01-01 22:47:41 +0100

    Locale from Accept-Language request header

--- rawp/src/main/java/rest/api/web/provider/exception/GlobalExceptionHandler.java
+++ rawp/src/main/java/rest/api/web/provider/exception/GlobalExceptionHandler.java
@@ -2,12 +2,15 @@ package rest.api.web.provider.exception;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.Locale;
+import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,7 +19,7 @@ public class GlobalExceptionHandler {
     MessageSource messageSource;
 
     @ExceptionHandler({RAWPException.class})
-    public ResponseEntity<Object> handleRAWPException(RAWPException exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageSource.getMessage(exception.getErrorCode(), null, "RAWPException is thrown", new Locale("sr")));
+    public ResponseEntity<Object> handleRAWPException(RAWPException exception, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageSource.getMessage(exception.getErrorCode(), null, "RAWPException is thrown", new Locale(Objects.requireNonNull(request.getHeader(HttpHeaders.ACCEPT_LANGUAGE)))));
     }
 }
